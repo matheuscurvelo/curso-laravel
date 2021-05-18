8600cf88ddd61f1ac9e022829bcb90edd7b2c0a2
Matheus Curvelo <38927923+matheuscurvelo@users.noreply.github.com>
2021-05-18 08:56:57 -0300

    Factories

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        print_r($_POST);
+        //1ª forma
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+
+        //2ª forma
+        $contato2 = new SiteContato();
+
+        $contato2->fill($request->all());
+        //ou
+        $contato2->create($request->all());
+
+        $contato2->save();
         return view('site.contato');
     }
 }
