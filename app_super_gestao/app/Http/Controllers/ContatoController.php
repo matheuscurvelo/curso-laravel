eb55f8e38cc75d06b458289fed9e71b384d44086
Matheus Curvelo <38927923+matheuscurvelo@users.noreply.github.com>
2021-05-20 08:38:04 -0300

    feedback

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -18,17 +18,24 @@ class ContatoController extends Controller
     public function salvar(Request $request)
     {
         //validar os dados
-        $request->validate(
-            [
-                'nome' => 'required|min:3|max:40',
-                'telefone' => 'required',
-                'email' => 'email',
-                'motivo_contatos_id' => 'required',
-                'mensagem' => 'required',
-            ],[
-                'nome.required' =>  
-            ]
-        );
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required'
+        ];
+        $feedback = [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'nome.min' => 'O campo nome precisa ter pelo menos 3 caracteres',
+            'nome.max' => 'O campo nome pode ter até 40 caracteres',
+            'nome.unique' => 'O nome informado ja está em uso',
+            'email.email' => 'O email informado não é valido',
+            'mensagem.max' => 'A mensagem pode ter até 2000 caracteres',
+        ];
+
+        $request->validate($regras,$feedback);
+        
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
